04d70953f3c15d136bb54a4a43089c6d5f1c49b9
lizaf999 <37180011+lizaf999@users.noreply.github.com>
2020-02-23 17:56:46 +0900

    renderTarget works

---
+++ Fluid2D/index.js
@@ -0,0 +1,80 @@
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  const width = 960;
+  const height = 540;
+
+  var renderTarget = new THREE.WebGLRenderTarget(width,width,{
+    magFilter:THREE.NearestFilter,
+    minFilter:THREE.NearestFilter,
+    wrapS:THREE.ClampToEdgeWrapping,
+    wrapT:THREE.ClampToEdgeWrapping,
+    depthBuffer:false,
+    stencilBuffer:false
+  })
+
+  // レンダラーを作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#myCanvas")
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+
+  // シーンを作成
+  const scene = new THREE.Scene();
+
+  // カメラを作成
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    width / height,
+    1,
+    10000
+  );
+  camera.position.set(0, 0, +1000);
+
+  // 箱を作成
+  const geometry = new THREE.BoxGeometry(500, 500, 500);
+  const material = new THREE.MeshStandardMaterial({
+    map:renderTarget.texture
+  });
+  const box = new THREE.Mesh(geometry, material);
+  scene.add(box);
+
+  // 平行光源
+  const light = new THREE.DirectionalLight(0xffffff);
+  light.intensity = 2; // 光の強さを倍に
+  light.position.set(1, 1, 1);
+  // シーンに追加
+  scene.add(light);
+
+  const scenePre = new THREE.Scene()
+  const materialPre = new THREE.MeshStandardMaterial({
+    color:0xffff00
+  });
+  const boxPre = new THREE.Mesh(geometry, materialPre);
+  const lightPre = new THREE.DirectionalLight(0xffffff);
+  lightPre.intensity = 2; // 光の強さを倍に
+  lightPre.position.set(1, 1, 1);
+  scenePre.add(boxPre);
+  scenePre.add(lightPre)
+
+
+  // 初回実行
+  tick();
+
+  function tick() {
+    requestAnimationFrame(tick);
+
+    // 箱を回転させる
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.01;
+    boxPre.rotation.x += 0.03;
+    boxPre.rotation.y += 0.07;
+
+    // レンダリング
+    renderer.setRenderTarget(renderTarget)
+    renderer.render(scenePre, camera)
+    renderer.setRenderTarget(null)
+    renderer.render(scene, camera);
+  }
+}

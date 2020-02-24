60e090fa8a268299af39edcc79f0ea93b1cbad43
lizaf999 <37180011+lizaf999@users.noreply.github.com>
2020-02-24 17:25:31 +0900

    add webpack

--- Fluid2D/index.js
+++ Fluid2D/index.js
@@ -1,80 +1,100 @@
-window.addEventListener("DOMContentLoaded", init);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./main.ts");
+/******/ })
+/************************************************************************/
+/******/ ({
 
-function init() {
-  const width = 960;
-  const height = 540;
+/***/ "./main.ts":
+/*!*****************!*\
+  !*** ./main.ts ***!
+  \*****************/
+/*! no static exports found */
+/***/ (function(module, exports) {
 
-  var renderTarget = new THREE.WebGLRenderTarget(width,width,{
-    magFilter:THREE.NearestFilter,
-    minFilter:THREE.NearestFilter,
-    wrapS:THREE.ClampToEdgeWrapping,
-    wrapT:THREE.ClampToEdgeWrapping,
-    depthBuffer:false,
-    stencilBuffer:false
-  })
+eval("window.addEventListener(\"DOMContentLoaded\", init);\nfunction init() {\n    var width = 960;\n    var height = 540;\n    var renderTarget = new THREE.WebGLRenderTarget(width, width, {\n        magFilter: THREE.NearestFilter,\n        minFilter: THREE.NearestFilter,\n        wrapS: THREE.ClampToEdgeWrapping,\n        wrapT: THREE.ClampToEdgeWrapping,\n        depthBuffer: false,\n        stencilBuffer: false\n    });\n    // レンダラーを作成\n    var renderer = new THREE.WebGLRenderer({\n        canvas: document.querySelector(\"#myCanvas\")\n    });\n    renderer.setPixelRatio(window.devicePixelRatio);\n    renderer.setSize(width, height);\n    // シーンを作成\n    var scene = new THREE.Scene();\n    // カメラを作成\n    var camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);\n    camera.position.set(0, 0, +1000);\n    // 箱を作成\n    var geometry = new THREE.BoxGeometry(500, 500, 500);\n    var material = new THREE.MeshStandardMaterial({\n        map: renderTarget.texture\n    });\n    var box = new THREE.Mesh(geometry, material);\n    scene.add(box);\n    // 平行光源\n    var light = new THREE.DirectionalLight(0xffffff);\n    light.intensity = 2; // 光の強さを倍に\n    light.position.set(1, 1, 1);\n    // シーンに追加\n    scene.add(light);\n    var scenePre = new THREE.Scene();\n    var materialPre = new THREE.MeshStandardMaterial({\n        color: 0xffff00\n    });\n    var boxPre = new THREE.Mesh(geometry, materialPre);\n    var lightPre = new THREE.DirectionalLight(0xffffff);\n    lightPre.intensity = 2; // 光の強さを倍に\n    lightPre.position.set(1, 1, 1);\n    scenePre.add(boxPre);\n    scenePre.add(lightPre);\n    // 初回実行\n    tick();\n    function tick() {\n        requestAnimationFrame(tick);\n        // 箱を回転させる\n        box.rotation.x += 0.01;\n        box.rotation.y += 0.01;\n        boxPre.rotation.x += 0.03;\n        boxPre.rotation.y += 0.05;\n        // レンダリング\n        renderer.setRenderTarget(renderTarget);\n        renderer.render(scenePre, camera);\n        renderer.setRenderTarget(null);\n        renderer.render(scene, camera);\n    }\n}\n\n\n//# sourceURL=webpack:///./main.ts?");
 
-  // レンダラーを作成
-  const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector("#myCanvas")
-  });
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(width, height);
+/***/ })
 
-  // シーンを作成
-  const scene = new THREE.Scene();
-
-  // カメラを作成
-  const camera = new THREE.PerspectiveCamera(
-    45,
-    width / height,
-    1,
-    10000
-  );
-  camera.position.set(0, 0, +1000);
-
-  // 箱を作成
-  const geometry = new THREE.BoxGeometry(500, 500, 500);
-  const material = new THREE.MeshStandardMaterial({
-    map:renderTarget.texture
-  });
-  const box = new THREE.Mesh(geometry, material);
-  scene.add(box);
-
-  // 平行光源
-  const light = new THREE.DirectionalLight(0xffffff);
-  light.intensity = 2; // 光の強さを倍に
-  light.position.set(1, 1, 1);
-  // シーンに追加
-  scene.add(light);
-
-  const scenePre = new THREE.Scene()
-  const materialPre = new THREE.MeshStandardMaterial({
-    color:0xffff00
-  });
-  const boxPre = new THREE.Mesh(geometry, materialPre);
-  const lightPre = new THREE.DirectionalLight(0xffffff);
-  lightPre.intensity = 2; // 光の強さを倍に
-  lightPre.position.set(1, 1, 1);
-  scenePre.add(boxPre);
-  scenePre.add(lightPre)
-
-
-  // 初回実行
-  tick();
-
-  function tick() {
-    requestAnimationFrame(tick);
-
-    // 箱を回転させる
-    box.rotation.x += 0.01;
-    box.rotation.y += 0.01;
-    boxPre.rotation.x += 0.03;
-    boxPre.rotation.y += 0.07;
-
-    // レンダリング
-    renderer.setRenderTarget(renderTarget)
-    renderer.render(scenePre, camera)
-    renderer.setRenderTarget(null)
-    renderer.render(scene, camera);
-  }
-}
+/******/ });
---
+++ Fluid2D/main.js
@@ -0,0 +1,63 @@
+window.addEventListener("DOMContentLoaded", init);
+function init() {
+    var width = 960;
+    var height = 540;
+    var renderTarget = new THREE.WebGLRenderTarget(width, width, {
+        magFilter: THREE.NearestFilter,
+        minFilter: THREE.NearestFilter,
+        wrapS: THREE.ClampToEdgeWrapping,
+        wrapT: THREE.ClampToEdgeWrapping,
+        depthBuffer: false,
+        stencilBuffer: false
+    });
+    // レンダラーを作成
+    var renderer = new THREE.WebGLRenderer({
+        canvas: document.querySelector("#myCanvas")
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+    // シーンを作成
+    var scene = new THREE.Scene();
+    // カメラを作成
+    var camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+    camera.position.set(0, 0, +1000);
+    // 箱を作成
+    var geometry = new THREE.BoxGeometry(500, 500, 500);
+    var material = new THREE.MeshStandardMaterial({
+        map: renderTarget.texture
+    });
+    var box = new THREE.Mesh(geometry, material);
+    scene.add(box);
+    // 平行光源
+    var light = new THREE.DirectionalLight(0xffffff);
+    light.intensity = 2; // 光の強さを倍に
+    light.position.set(1, 1, 1);
+    // シーンに追加
+    scene.add(light);
+    var scenePre = new THREE.Scene();
+    var materialPre = new THREE.MeshStandardMaterial({
+        color: 0xffff00
+    });
+    var boxPre = new THREE.Mesh(geometry, materialPre);
+    var lightPre = new THREE.DirectionalLight(0xffffff);
+    lightPre.intensity = 2; // 光の強さを倍に
+    lightPre.position.set(1, 1, 1);
+    scenePre.add(boxPre);
+    scenePre.add(lightPre);
+    // 初回実行
+    tick();
+    function tick() {
+        requestAnimationFrame(tick);
+        // 箱を回転させる
+        box.rotation.x += 0.01;
+        box.rotation.y += 0.01;
+        boxPre.rotation.x += 0.03;
+        boxPre.rotation.y += 0.01;
+        // レンダリング
+        renderer.setRenderTarget(renderTarget);
+        renderer.render(scenePre, camera);
+        renderer.setRenderTarget(null);
+        renderer.render(scene, camera);
+    }
+}
+//# sourceMappingURL=main.js.map
